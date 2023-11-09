ad9f56a4c6932673bfba6872260b452b7e93abde
AnasPirzada <anaspirzada510@gmail.com>
2023-11-09 16:32:25 +0500

    commit

--- src/Pages/User Repport/pages/userControl.jsx
+++ src/Pages/User Repport/pages/userControl.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ResponsiveExample() {
   const tableData = [
     {
       name: 'Criss Germano',
-      Date: 'Set 29, 2023 <br/> At 08.00 PM',
+      Date: 'Set 29, 2023 At 08.00 PM',
+      Time: 'At 08.00 PM',
       UserId: '@crissgermano',
       Local: 'MA',
       Actions: 'Enable',
     },
   ];
+  const [selectedValue, setSelectedValue] = useState('');
 
+  const handleDropdownChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
   return (
     <div
       className='mt-4 m-auto '
@@ -51,7 +56,10 @@ function ResponsiveExample() {
                     src='./image/userKellyimage.png'
                     style={{ width: '40px' }}
                   />
-                  <img src='./table dull active.svg' style={{ transform:'translateY() translateX(50px)'}}/>
+                  <img
+                    src='./table dull active.svg'
+                    style={{ transform: 'translateY(-10px) translateX(-10px)' }}
+                  />
                 </div>
 
                 <p className='m-0 pt-1 ms-3'>{data.name}</p>
@@ -59,7 +67,25 @@ function ResponsiveExample() {
               <td className='text-secondary'>{data.Date}</td>
               <td className='text-secondary'>{data.UserId}</td>
               <td className='text-secondary'>{data.Local}</td>
-              <td className='text-secondary'>{data.Actions}</td>
+              <tr>
+                <td className='text-secondary'>{selectedValue}</td>
+                <td>
+                  <select
+                    className='input-with-icon'
+                    style={{
+                      paddingLeft: '42px',
+                      backgroundColor: '#fff !important',
+                      outline: 'none',
+                    }}
+                    value={selectedValue}
+                    onChange={handleDropdownChange}
+                  >
+                    <option value=''>Bed & Bath</option>
+                    <option value='option1'>Option 1</option>
+                    <option value='option2'>Option 2</option>
+                  </select>
+                </td>
+              </tr>
             </tr>
           ))}
         </tbody>
