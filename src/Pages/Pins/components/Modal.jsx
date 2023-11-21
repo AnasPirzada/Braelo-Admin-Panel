aea57967b43d98e3d82f0248b68100b613d5d46e
AnasPirzada <anaspirzada510@gmail.com>
2023-11-21 11:42:02 +0500

    commit

---
+++ src/Pages/Pins/components/Modal.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './PinsPreviewModal.css';
+const images = [
+  'https://plus.unsplash.com/premium_photo-1664047543348-1536f15d248c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1622737133809-d95047b9e673?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1493514789931-586cb221d7a7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1505832018823-50331d70d237?q=80&w=2108&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1475257026007-0753d5429e10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+];
+export const Modal = () => {
+  return (
+    <div>
+      <Carousel
+        controls={false}
+        indicators={true}
+        fade={false}
+        interval={null}
+        className='custom-carousel'
+      >
+        {images.map((imageUrl, index) => (
+          <Carousel.Item key={index}>
+            <img
+              src={imageUrl}
+              alt={`Slide ${index + 1}`}
+              className='d-block w-100'
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+export default Modal;
--- src/Pages/Pins/components/PinsPreviewModal.jsx
+++ src/Pages/Pins/components/PinsPreviewModal.jsx
@@ -1,42 +1,10 @@
-import React, { useState } from 'react';
-import './Modal.css';
-
+import React from 'react';
+import PinsModal from './PinsPreviewModal.jsx';
 export const PinsPreviewModal = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const images = [
-    'https://plus.unsplash.com/premium_photo-1664047543348-1536f15d248c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1622737133809-d95047b9e673?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1493514789931-586cb221d7a7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1505832018823-50331d70d237?q=80&w=2108&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1475257026007-0753d5429e10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ];
-  const changeImage = index => {
-    setActiveIndex(index);
-  };
-
   return (
-    <div className='carousel-container'>
-      <div className='carousel-images'>
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Image ${index + 1}`}
-            className={index === activeIndex ? 'active' : 'hidden'}
-          />
-        ))}
-      </div>
-      <div className='carousel-dots'>
-        {images.map((_, index) => (
-          <span
-            key={index}
-            className={index === activeIndex ? 'dot active-dot' : 'dot'}
-            onClick={() => changeImage(index)}
-          />
-        ))}
-      </div>
+    <div>
+      <PinsModal />
     </div>
   );
 };
-
 export default PinsPreviewModal;
