fb41ca5746d3a905c097c2d80d8bc30ef677eeff
AnasPirzada <anaspirzada510@gmail.com>
2023-11-21 14:33:52 +0500

    commit

--- src/Pages/Pins/components/PinsPreviewModal.jsx
+++ src/Pages/Pins/components/PinsPreviewModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PinsModal from './pinsModal.jsx';
+import { Row, Col } from 'react-bootstrap';
 export const PinsPreviewModal = () => {
   return (
     <div
@@ -11,6 +12,56 @@ export const PinsPreviewModal = () => {
       }}
     >
       <PinsModal />
+
+      <Row className='d-flex justify-content-between align-items-center mt-5 m-auto '>
+        <Col xl={4} className='mt-4'>
+          <button
+            className='w-100 p-3 rounded-5'
+            style={{
+              color: '#475467',
+              border: '1px solid #EAECF0',
+              backgroundColor: 'transparent',
+            }}
+          >
+            <img src='./cards/location.svg' alt='' className='me-3' />
+            Miami,Fl
+          </button>
+        </Col>
+        <Col xl={5}></Col>
+        <Col xl={3} className='mt-4'>
+          <p className='my-0'>10 July 2023</p>
+        </Col>
+      </Row>
+      <div
+        className='m-auto'
+        style={{
+          width: '90%',
+        }}
+      >
+        <div className='mt-4'>
+          <p className='fs-2' style={{ fontWeight: '600', color: '#101828' }}>
+            Lide Business Group
+            <br /> Florida
+          </p>
+          <p style={{ color: '#667085', fontSize: '21.41px' }}>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            ...
+          </p>
+        </div>
+        <div className='mt-4'>
+          <p style={{ color: '#1D96FF', fontSize: '21.41px' }}>Read more</p>
+        </div>
+        <div>
+          <p>Endereço</p>
+        </div>
+        <div >
+          <p>City Hall Square, Florida | Miami | EUA</p>
+        </div>
+        <div>
+          <p>City Hall Square, Florida | Miami | EUA</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Pins/components/pinsModal.jsx
+++ src/Pages/Pins/components/pinsModal.jsx
@@ -6,7 +6,10 @@ const images = [
   'https://images.unsplash.com/photo-1622737133809-d95047b9e673?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1493514789931-586cb221d7a7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1505832018823-50331d70d237?q=80&w=2108&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1475257026007-0753d5429e10?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+  'https://images.unsplash.com/photo-1700514351733-992fc7b66306?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1700496462947-f772a716de6f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 ];
 export const pinsModal = () => {
   return (
@@ -20,12 +23,38 @@ export const pinsModal = () => {
       >
         {images.map((imageUrl, index) => (
           <Carousel.Item key={index}>
-            <img
+            {/* <img
               style={{ borderRadius: ' 12px 12px 0 0 ', height: '387px' }}
               src={imageUrl}
               alt={`Slide ${index + 1}`}
               className='d-block w-100'
             />
+            <img src='./braeloqr-code 1.png' alt='' /> */}
+            <div style={{ position: 'relative', display: 'inline-block' }}>
+              <img
+                style={{
+                  borderRadius: '12px 12px 0 0',
+                  height: '387px',
+                  width: '610px',
+                }}
+                src={imageUrl}
+                alt={`Slide ${index + 1}`}
+                // className='d-block w-100'
+              />
+              <img
+                src='./braeloqr-code 1.png'
+                alt=''
+                style={{
+                  position: 'absolute',
+                  bottom: '0',
+                  right: '0',
+                  marginRight: '10px',
+                  marginBottom: '10px',
+                  maxWidth: '100px', // Adjust the size as needed
+                  maxHeight: '100px', // Adjust the size as needed
+                }}
+              />
+            </div>
           </Carousel.Item>
         ))}
       </Carousel>
